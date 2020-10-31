d43d812d012a3abdbd8fa83c802cb6cb094a3a47
Sheikh MiZan <sheikhmizanbd@gmail.com>
2020-10-31 23:54:16 +0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,1 @@
-  $("#seelc").attr("src" , "https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&controls=0&fs=0&loop=1&modestbranding=1&playsinline=1&iv_load_policy=3");
+  $("#seelc").attr("src" , "https://www.youtube.com/embed/M7lc1UVf-VE?rel=0&loop=""1&autoplay=1&mute=1");
