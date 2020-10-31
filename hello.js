a4c7992d014a6a6dd8a7f97d24a3a06e63f06876
Sheikh MiZan <sheikhmizanbd@gmail.com>
2020-10-31 23:57:17 +0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-  $("#seelc").attr("src" , "https://www.youtube.com/embed/M7lc1UVf-VE?rel=0&loop=1&autoplay=1&mute=1");
+<link href="css/YTPlayer.css" media="all" rel="stylesheet" type="text/css">
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+<script src="inc/jquery.mb.YTPlayer.js"></script>
+
+
+jQuery(function(){
+      jQuery("#selects").YTPlayer();
+    });
